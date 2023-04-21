8656dae26063aa572e24e814a3556c29e6ac5939
CatmanFan <oussama-studying@hotmail.com>
2023-04-21 02:00:07 +0000

    Test commit

---
+++ FriishProduce/main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FriishProduce
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+    }
+}
