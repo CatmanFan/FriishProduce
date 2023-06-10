04326801cfd88477f461f9d3c571d19fcbc835bb
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2023-06-09 22:42:01 -0500

    reimplement libWiiSharp for Flash WAD U8 handling
    fixes crashes on HOME Menu and other weird crashes on injected Flash game WADs produced with FriishProduce.

--- FriishProduce/main.cs
+++ FriishProduce/main.cs
@@ -1368,7 +1368,8 @@ namespace FriishProduce
                 // ----------------------------------------------------
                 else if (currentConsole == Platforms.Flash)
                 {
-                    await Task.Run(() => { U8.Unpack(Paths.WorkingFolder + "00000002.app", Paths.WorkingFolder_Content2); });
+                    // we're using libWiiSharp for unpack the U8 files on Flash WADs because Wii.cs breaks them...
+                    await Task.Run(() => { U8.libWiiSharpUnpack(Paths.WorkingFolder + "00000002.app", Paths.WorkingFolder_Content2); });
 
                     Injectors.Flash Flash = new Injectors.Flash() { SWF = input[0] };
                     Flash.ReplaceSWF();
@@ -1381,7 +1382,8 @@ namespace FriishProduce
                     if (Custom.Checked && tImg.Get()) tImg.CreateSave(Platforms.Flash);
                     if (Custom.Checked) Flash.InsertSaveData(SaveDataTitle.Lines);
 
-                    await Task.Run(() => { U8.Pack(Paths.WorkingFolder_Content2, Paths.WorkingFolder + "00000002.app"); });
+                    // we're using once again libWiiSharp for repack the U8 files on Flash WADs because Wii.cs breaks them...
+                    await Task.Run(() => { U8.libWiiSharpPack(Paths.WorkingFolder_Content2, Paths.WorkingFolder + "00000002.app"); });
                 }
 
                 // ----------------------------------------------------
