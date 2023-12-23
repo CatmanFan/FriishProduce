b1e368f747086cfec1f32fb82bacb7fe8e5ac12f
CatmanFan <oussama-studying@hotmail.com>
2023-12-24 00:15:52 +0100

    New version+year increment... stay tuned!

--- FriishProduce/Properties/AssemblyInfo.cs
+++ FriishProduce/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FriishProduce")]
-[assembly: AssemblyCopyright("Copyright © CatmanFan (Mr. Lechkar) 2023")]
+[assembly: AssemblyCopyright("Copyright © CatmanFan (Mr. Lechkar) 2023-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.*")]
-[assembly: AssemblyFileVersion("0.26")]
+[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyFileVersion("0.30")]
