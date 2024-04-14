f3571d759e5efebecaa4fef050c744518cb817ff
CatmanFan <oussama-studying@hotmail.com>
2024-04-14 02:41:28 +0100

    Test2

--- FriishProduce/_classes/Utilities.cs
+++ FriishProduce/_classes/Utilities.cs
@@ -30,6 +30,7 @@ namespace FriishProduce
 
         public static byte[] FromHex(string hex)
         {
+            hex = hex.ToUpper().Replace("-", "").Replace(" ", "");
             return Enumerable.Range(0, hex.Length)
                          .Where(x => x % 2 == 0)
                          .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
