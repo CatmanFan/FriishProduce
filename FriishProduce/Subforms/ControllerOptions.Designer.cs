59e3f926c2ffc754515b21d736da7e4902ef211a
CatmanFan <oussama-studying@hotmail.com>
2024-11-18 10:59:38 +0100

    Controller Options testform

---
+++ FriishProduce/Subforms/ControllerOptions.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace FriishProduce.Subforms
+{
+    partial class ControllerOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ControllerOptions));
+            this.bottomPanel2 = new System.Windows.Forms.Panel();
+            this.bottomPanel1 = new System.Windows.Forms.Panel();
+            this.b_cancel = new System.Windows.Forms.Button();
+            this.b_ok = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.bottomPanel2.SuspendLayout();
+            this.bottomPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bottomPanel2
+            // 
+            this.bottomPanel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.bottomPanel2.Controls.Add(this.bottomPanel1);
+            resources.ApplyResources(this.bottomPanel2, "bottomPanel2");
+            this.bottomPanel2.Name = "bottomPanel2";
+            // 
+            // bottomPanel1
+            // 
+            this.bottomPanel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.bottomPanel1.Controls.Add(this.b_cancel);
+            this.bottomPanel1.Controls.Add(this.b_ok);
+            resources.ApplyResources(this.bottomPanel1, "bottomPanel1");
+            this.bottomPanel1.Name = "bottomPanel1";
+            // 
+            // b_cancel
+            // 
+            this.b_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.b_cancel, "b_cancel");
+            this.b_cancel.Name = "b_cancel";
+            this.b_cancel.Tag = "b_cancel";
+            this.b_cancel.UseVisualStyleBackColor = true;
+            // 
+            // b_ok
+            // 
+            resources.ApplyResources(this.b_ok, "b_ok");
+            this.b_ok.Name = "b_ok";
+            this.b_ok.Tag = "b_ok";
+            this.b_ok.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // ControllerOptions
+            // 
+            this.AcceptButton = this.b_ok;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.CancelButton = this.b_cancel;
+            resources.ApplyResources(this, "$this");
+            this.ControlBox = false;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.bottomPanel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ControllerOptions";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Tag = "controller_options";
+            this.bottomPanel2.ResumeLayout(false);
+            this.bottomPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel bottomPanel2;
+        protected System.Windows.Forms.Panel bottomPanel1;
+        protected System.Windows.Forms.Button b_cancel;
+        protected System.Windows.Forms.Button b_ok;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ FriishProduce/Subforms/ControllerOptions.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FriishProduce.Subforms
+{
+    public partial class ControllerOptions : Form
+    {
+        public enum Buttons
+        {
+            WiiRemote_Up,
+            WiiRemote_Left,
+            WiiRemote_Right,
+            WiiRemote_Down,
+            WiiRemote_A,
+            WiiRemote_B,
+            WiiRemote_1,
+            WiiRemote_2,
+            WiiRemote_Plus,
+            WiiRemote_Minus,
+            WiiRemote_Home,
+            Nunchuck_Z,
+            Nunchuck_C,
+            Classic_Up,
+            Classic_Left,
+            Classic_Right,
+            Classic_Down,
+            Classic_A,
+            Classic_B,
+            Classic_X,
+            Classic_Y,
+            Classic_L,
+            Classic_R,
+            Classic_ZL,
+            Classic_ZR,
+            Classic_Plus,
+            Classic_Minus,
+            Classic_Home,
+            GC_Up,
+            GC_Left,
+            GC_Right,
+            GC_Down,
+            GC_A,
+            GC_B,
+            GC_X,
+            GC_Y,
+            GC_L,
+            GC_R,
+            GC_Z,
+            GC_Start,
+            GC_Select
+        };
+
+        public ControllerOptions()
+        {
+            InitializeComponent();
+        }
+
+        public IDictionary<Buttons, string> Mapping = new Dictionary<Buttons, string>()
+        {
+            { Buttons.WiiRemote_Left, null },
+            { Buttons.WiiRemote_Right, null },
+            { Buttons.WiiRemote_Down, null },
+            { Buttons.WiiRemote_Up, null },
+            { Buttons.WiiRemote_A, null },
+            { Buttons.WiiRemote_B, null },
+            { Buttons.WiiRemote_Home, null },
+            { Buttons.WiiRemote_Plus, null },
+            { Buttons.WiiRemote_Minus, null },
+            { Buttons.WiiRemote_1, null },
+            { Buttons.WiiRemote_2, null },
+            { Buttons.Nunchuck_Z, null },
+            { Buttons.Nunchuck_C, null },
+            { Buttons.Classic_Up, null },
+            { Buttons.Classic_Left, null },
+            { Buttons.Classic_ZR, null },
+            { Buttons.Classic_X, null },
+            { Buttons.Classic_A, null },
+            { Buttons.Classic_Y, null },
+            { Buttons.Classic_B, null },
+            { Buttons.Classic_ZL, null },
+            { Buttons.Classic_R, null },
+            { Buttons.Classic_Plus, null },
+            { Buttons.Classic_Home, null },
+            { Buttons.Classic_Minus, null },
+            { Buttons.Classic_L, null },
+            { Buttons.Classic_Down, null },
+            { Buttons.Classic_Right, null }
+        };
+
+        protected void OK_Click(object sender, EventArgs e)
+        {
+            if (DesignMode) return;
+
+            DialogResult = DialogResult.OK;
+        }
+
+        protected void Cancel_Click(object sender, EventArgs e)
+        {
+            if (DesignMode) return;
+
+            DialogResult = DialogResult.Cancel;
+        }
+
+        protected void Form_Load(object sender, EventArgs e)
+        {
+            if (DesignMode) return;
+
+            CenterToParent();
+        }
+    }
+}
